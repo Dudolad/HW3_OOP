75bf2ccebaca091c3799bd9322a5efb4d89e1cf7
dudolad_yaroslav <dudolad2009@gmail.com>
2025-08-18 00:06:39 +0300

    Created class "Computer", a default constructor, a parameterized constructor, and a destructor

--- HW3_OOP/HW3_OOP.cpp
+++ HW3_OOP/HW3_OOP.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>	
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Computer {
+public:
+	string name;
+	double frequancy;
+	int ram;
+	bool dvd;
+	double price;
+
+	Computer() : name(""), frequancy(0.0), ram(0), dvd(false), price(0.0) {}
+	Computer(string n, double f, int r, bool d, double p) 
+		: name(n), frequancy(f), ram(r), dvd(d), price(p) {
+	}
+	~Computer() {}
+};
