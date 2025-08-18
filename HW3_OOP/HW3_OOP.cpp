b7e3f76d5ece5d267029878db62db8492c322fca
dudolad_yaroslav <dudolad2009@gmail.com>
2025-08-18 20:18:27 +0300

    finished main

--- HW3_OOP/HW3_OOP.cpp
+++ HW3_OOP/HW3_OOP.cpp
@@ -32,3 +32,79 @@ void printVector(const vector<Computer>& comps) {
 	}
 	cout << "=========================" << endl;
 }
+
+int main()
+{
+	vector<Computer> computers = {
+		{"Lenovo", 3.2, 8, true, 500},
+		{"HP", 2.4, 16, false, 450},
+		{"Dell", 1.8, 4, true, 300},
+		{"Asus", 3.5, 32, true, 800},
+		{"Acer", 2.9, 16, true, 600}
+	};
+
+
+	cout << "Initial list of computers:" << endl;
+	printVector(computers);
+
+	string searchName = "Dell";
+	auto it = find_if(computers.begin(), computers.end(),
+		[&](const Computer& c) { return c.name == searchName; });
+
+	if (it != computers.end()) {
+		cout << "Computer found by name \"" << searchName << "\":" << endl;
+		it->print();
+	}
+	else {
+		cout << "Computer by name \"" << searchName << "\" not found." << endl;
+	}
+
+
+	auto minIt = min_element(
+		computers.begin(),
+		computers.end(),
+		[](const Computer& a, const Computer& b) { return a.frequancy < b.frequancy; });
+
+		if (minIt != computers.end())
+		{
+			cout << "\nThe computer with the lowest frequency: " << endl;
+			minIt->print();
+			computers.erase(minIt);
+		}
+		cout << "\nList of computers after removing the one with the lowest frequency:" << endl;
+		printVector(computers);
+
+
+	int countDVD = count_if(computers.begin(), computers.end(),
+		[](const Computer& c) { return c.dvd; });
+
+	cout << "Number of computers with DVD: " << countDVD << endl;
+
+
+	for_each(computers.begin(), computers.end(),
+		[](Computer& c) {
+			if (c.ram > 16) c.price *= 1.1;
+		});
+
+	cout << "\nПісля збільшення ціни (RAM > 16):" << endl;
+	printVector(computers);
+
+
+	sort(computers.begin(), computers.end(),
+		[](const Computer& a, const Computer& b) { return a.price < b.price; });
+
+	cout << "Сортування за зростанням ціни:" << endl;
+	printVector(computers);
+
+
+
+	sort(computers.begin(), computers.end(),
+		[](const Computer& a, const Computer& b) { return a.frequancy > b.frequancy; });
+
+	cout << "Сортування за спаданням ціни:" << endl;
+	printVector(computers);
+
+	return 0;
+}
+
+
